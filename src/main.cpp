8b0eed69c87f0f873a5b0bdb3606b483bab78cf8
Mohammad Talim <talimmohammad116@gmail.com>
2024-12-04 18:55:18 +0530

    implement the echo builtin

--- src/main.cpp
+++ src/main.cpp
@@ -10,23 +10,27 @@ void exit_builtin() {
   exit(0);
 }
 
+void echo_builtin(std::string argument) {
+  std::cout << argument << std::endl;
+}
+
 int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
-  std::unordered_map<std::string, std::function<void()>> commands = {
-    { "exit 0", exit_builtin },
-  };
 
   while (true) {
     std::cout << "$ ";
     std::string input;
     std::getline(std::cin, input);
 
-    auto command = commands.find(input);
+    int space_index = input.find(' ');
+    std::string command = input.substr(0, space_index);
+    std::string arguments = input.substr(space_index + 1);
 
-    if (command != commands.end()) {
-      command->second();
+    if (command == "echo") {
+      echo_builtin(arguments);
+    } else if (command == "exit") {
+      exit_builtin();
     } else {
       handle_invalid_command(input);
     }
