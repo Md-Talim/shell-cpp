37fcc8b58ca2cc06a2f46a79d801dc7583585273
Mohammad Talim <talimmohammad116@gmail.com>
2024-12-04 18:25:16 +0530

    implement support for handling invalid commands

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <unordered_map>
+#include <functional>
+
+void handle_invalid_command(std::string command) {
+  std::cout << command << ": command not found" << std::endl;
+}
 
 int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
-  std::string input;
-  std::getline(std::cin, input);
+    std::unordered_map<std::string, std::function<void()>> commands = {};
+
+  while (true) {
+    std::cout << "$ ";
+    std::string input;
+    std::getline(std::cin, input);
+
+    auto command = commands.find(input);
+
+    if (command != commands.end()) {
+      command->second();
+    } else {
+      handle_invalid_command(input);
+    }
+  }
 }
