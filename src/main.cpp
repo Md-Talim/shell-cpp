a620b6e8dd6fb1fc9b149f29bd03b5956697c646
Mohammad Talim <talimmohammad116@gmail.com>
2024-12-08 08:06:18 +0530

    refactor: split code into reusable functions

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,12 @@
 #include <string>
 #include <vector>
 
+char delimiter = ':';
+
+#ifdef _WIN32
+char delimiter = ';';  // Use semicolon on Windows
+#endif
+
 void handle_invalid_command(std::string command) {
   std::cout << command << ": command not found" << std::endl;
 }
@@ -17,46 +23,17 @@ void exit_builtin() { exit(0); }
 void echo_builtin(std::string argument) { std::cout << argument << std::endl; }
 
 bool is_executable(const std::filesystem::path &file_path) {
-  return access(file_path.string().c_str(), X_OK) == 0;
+  return std::filesystem::exists(file_path) &&
+         std::filesystem::is_regular_file(file_path) &&
+         ((std::filesystem::status(file_path).permissions() &
+           std::filesystem::perms::owner_exec) != std::filesystem::perms::none);
 }
 
-void type_builtin(std::string command) {
-  if (command.empty()) {
-    std::cout << "type: missing argument" << std::endl;
-    return;
-  }
-
-  auto print_builtin = [command]() {
-    std::cout << command << " is a shell builtin" << std::endl;
-  };
-
-  auto print_executable = [command](std::string abs_path) {
-    std::cout << command << " is " << abs_path << std::endl;
-  };
-
-  auto print_not_found = [command]() {
-    std::cout << command << ": not found" << std::endl;
-  };
-
-  std::vector<std::string> builtin_commands = {"echo", "exit", "type"};
-  auto search_it =
-      std::find(builtin_commands.begin(), builtin_commands.end(), command);
-
-  // if the command is a builtin command
-  if (search_it != builtin_commands.end()) {
-    print_builtin();
-    return;
-  }
-
+std::string get_executable_path(std::string command) {
   std::string path = std::getenv("PATH");
   std::vector<std::string> directories;
   std::stringstream ss(path);
   std::string dir;
-  char delimiter = ':';
-
-#ifdef _WIN32
-  delimiter = ';';  // Use semicolon on Windows
-#endif
 
   while (std::getline(ss, dir, delimiter)) {
     directories.push_back(dir);
@@ -65,19 +42,38 @@ void type_builtin(std::string command) {
   for (const auto &dir : directories) {
     std::filesystem::path file_path = std::filesystem::path(dir) / command;
 
-    if (std::filesystem::exists(file_path) &&
-        std::filesystem::is_regular_file(file_path) &&
-        ((std::filesystem::status(file_path).permissions() &
-          std::filesystem::perms::owner_exec) !=
-         std::filesystem::perms::none)) {
-      // if the command is an executable command
-      print_executable(file_path.string());
-      return;
+    if (is_executable(file_path)) {
+      return file_path.string();
     }
   }
 
+  return "";
+}
+
+void type_builtin(std::string command) {
+  if (command.empty()) {
+    std::cout << "type: missing argument" << std::endl;
+    return;
+  }
+
+  std::vector<std::string> builtin_commands = {"echo", "exit", "type"};
+  auto search_it =
+      std::find(builtin_commands.begin(), builtin_commands.end(), command);
+
+  // if the command is a builtin command
+  if (search_it != builtin_commands.end()) {
+    std::cout << command << " is a shell builtin" << std::endl;
+    return;
+  }
+
+  std::string executable_path = get_executable_path(command);
+
+  if (executable_path != "") {
+    std::cout << command << " is " << executable_path << std::endl;
+  }
+
   // if the command is not found
-  print_not_found();
+  std::cout << command << ": not found" << std::endl;
 }
 
 int main() {
