67d2d2d1701afaa860237223e3ea1a03f3776526
Mohammad Talim <talimmohammad116@gmail.com>
2024-12-10 18:17:45 +0530

    fix: remove double quotes

--- src/builtins/pwd.cpp
+++ src/builtins/pwd.cpp
@@ -1,8 +1,14 @@
+#include <algorithm>
 #include <filesystem>
 #include <iostream>
 
 #include "builtins.h"
 
 void pwd_builtin() {
-    std::cout << std::filesystem::current_path() << std::endl;
+    std::filesystem::path current_path = std::filesystem::current_path();
+    std::string current_path_str = current_path.string();
+
+    std::replace(current_path_str.begin(), current_path_str.end(), '\\', '/');
+
+    std::cout << current_path_str << std::endl;
 }
