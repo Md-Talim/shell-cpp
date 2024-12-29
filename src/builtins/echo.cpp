7574c2ad51116fffcb145caffdefdab52ff46e74
Mohammad Talim <talimmohammad116@gmail.com>
2024-12-29 10:01:23 +0530

    fix: remove extra spaces without quotes

--- src/builtins/echo.cpp
+++ src/builtins/echo.cpp
@@ -2,10 +2,27 @@
 
 #include "builtins.h"
 
+std::string remove_extra_spaces(const std::string& str) {
+    std::string result;
+    bool space = false;
+    for (char c : str) {
+        if (c == ' ') {
+            if (!space) {
+                result += c;
+                space = true;
+            }
+        } else {
+            result += c;
+            space = false;
+        }
+    }
+    return result;
+}
+
 void echo_builtin(const std::string& argument) {
     if (argument[0] == '\'') {
         std::cout << argument.substr(1, argument.size() - 2) << std::endl;
     } else {
-        std::cout << argument << std::endl;
+        std::cout << remove_extra_spaces(argument) << std::endl;
     }
 }
