22af1da345065bbe2bade37b9adc50d20c4a37c5
Mohammad Talim <talimmohammad116@gmail.com>
2025-01-26 09:34:27 +0000

    implement support for executing a quoted executable

--- src/utils/command.cpp
+++ src/utils/command.cpp
@@ -4,13 +4,25 @@
 #include <iostream>
 
 #include "path.h"
+#include "string.h"
 
 std::tuple<std::string, std::string> parse_command(std::string& input) {
     int space_index = input.find(' ');
 
     if (space_index == std::string::npos) {
+        if (input[0] == '\'')
+            input = parse_single_quotes(input);
+        else if (input[0] == '\"')
+            input = parse_double_quotes(input);
+
         return {input, ""};
     } else {
+        std::string command = input.substr(0, space_index);
+        if (input[0] == '\'')
+            command = parse_single_quotes(command);
+        else if (input[0] == '\"')
+            command = parse_double_quotes(command);
+
         return {input.substr(0, space_index), input.substr(space_index + 1)};
     }
 }
